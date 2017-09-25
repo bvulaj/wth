daacbeddced307e1de587337dd5b3a9d80cae99b
Brandon Vulaj <brandon.vulaj@gmail.com>
2017-09-25 11:29:28 -0400

    Got it working with some caveats

--- src/main/java/com/redhat/it/wth/ServerVerticle.java
+++ src/main/java/com/redhat/it/wth/ServerVerticle.java
@@ -7,23 +7,21 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.StaticHandler;
 
 @Component
 public class ServerVerticle extends AbstractVerticle {
 
-	@Value("${http.port}")
+	@Value("${http.port:8080}")
 	private int port;
 
 	@Override
 	public void start() throws Exception {
-		vertx.createHttpServer().requestHandler(router()::accept).listen(port); // port
+		vertx.createHttpServer().requestHandler(router()::accept).listen(port);
 	}
 
 	private Router router() {
 		Router router = Router.router(vertx);
-		router.route().handler(StaticHandler.create());
-
+		// router.route().handler(StaticHandler.create()); // for some reason this causes other routes to not be served. will figure out later
 		router.route("/hello").handler(routingContext -> {
 			HttpServerResponse response = routingContext.response();
 			response.putHeader("Content-Type", "application/json");
