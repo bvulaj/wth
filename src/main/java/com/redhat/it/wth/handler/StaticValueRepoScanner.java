6736a5420aeb5e9c7611cfa103174c8ccd28857a
Josh Cain <jcain@redhat.com>
2017-09-25 12:32:37 -0500

    put value back in static value scanner

--- src/main/java/com/redhat/it/wth/handler/StaticValueRepoScanner.java
+++ src/main/java/com/redhat/it/wth/handler/StaticValueRepoScanner.java
@@ -3,17 +3,25 @@ package com.redhat.it.wth.handler;
 import com.redhat.it.wth.model.Repo;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class StaticValueRepoScanner implements RepoScanner {
 
-	private final Set<Repo> staticRepolist;
+	private Set<Repo> staticRepolist;
 
 	public StaticValueRepoScanner() {
-		staticRepolist = new HashSet<>();
+		try {
+			staticRepolist = Collections.singleton(new Repo("darcy", new URI("ssh://gitolite.corp.redhat.com/it-smw/darcy.git")));
+		} catch (URISyntaxException e) {
+			// TODO throw something
+			staticRepolist = Collections.emptySet();
+		}
 	}
 
 	public StaticValueRepoScanner(final Set<Repo> staticRepolist) {
