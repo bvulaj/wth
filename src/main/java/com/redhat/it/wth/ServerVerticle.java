b871379b6cd0fc095fd5f6a4468f2ba552263e83
Josh Cain <jcain@redhat.com>
2017-09-25 11:37:20 -0500

    un-qualify repo-handler (gasp)

--- src/main/java/com/redhat/it/wth/ServerVerticle.java
+++ src/main/java/com/redhat/it/wth/ServerVerticle.java
@@ -1,20 +1,14 @@
 package com.redhat.it.wth;
 
-import java.util.List;
-
 import com.redhat.it.wth.handler.RepoHandler;
-import io.vertx.core.Handler;
-import io.vertx.ext.web.RoutingContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ServerVerticle extends AbstractVerticle {
@@ -22,8 +16,8 @@ public class ServerVerticle extends AbstractVerticle {
 	@Value("${http.port:8080}")
 	private int port;
 
-	@Autowired @Qualifier("repo")
-	private Handler<RoutingContext> repoHandler;
+	@Autowired
+	private RepoHandler repoHandler;
 
 	@Override
 	public void start() throws Exception {
@@ -37,8 +31,6 @@ public class ServerVerticle extends AbstractVerticle {
 			response.putHeader("Content-Type", "application/json");
 			response.end(Json.encodePrettily("Hello World"));
 		});
-
-		// TODO autowire this, un-new.  ugh.
 		router.route("/repos").handler(repoHandler);
 		router.route().handler(StaticHandler.create());
 		return router;
