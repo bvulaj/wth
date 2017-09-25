fbdeb083e6e0dc74c2f75a5ba2f5d4d6d7bb44bb
Josh Cain <jcain@redhat.com>
2017-09-25 12:30:21 -0500

    have a basic vertx client call working from the gitolite scanner

--- src/main/java/com/redhat/it/wth/handler/GitoliteRepoScanner.java
+++ src/main/java/com/redhat/it/wth/handler/GitoliteRepoScanner.java
@@ -3,7 +3,10 @@ package com.redhat.it.wth.handler;
 
 import com.redhat.it.wth.model.Repo;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -19,14 +22,16 @@ public class GitoliteRepoScanner implements RepoScanner, NeedsVertx<GitoliteRepo
 	public Set<Repo> scanForRepos(final URL sourceUrl) {
 		// TODO for now we're just assuming gitolite organization URL's.  Need to validate prior to attempted parsing
 
-		WebClient client = WebClient.create(vertx);
-		client.get("https://gitolite.corp.redhat.com/cgit/it-smw")
+		WebClient client = WebClient.create(vertx, new WebClientOptions().setSsl(true));
+		client.get(443, "gitolite.corp.redhat.com", "/cgit/it-smw")
 				.send(result -> {
 					if (result.succeeded()) {
 						System.out.println("we have had great success!");
+						final HttpResponse<Buffer> response = result.result();
+						System.out.println(response.bodyAsString());
 					}
 					else {
-						System.out.println("FAILURE!");
+						System.out.println("FAILURE! " + result.cause().getMessage());
 					}
 				});
 
